23546c3f8179e15171fa9929cd99579a6be340a9
Singgih Pratama <singgihpratama96@gmail.com>
2023-09-05 06:43:30 +0700

    initialize project

---
+++ src/main/java/tea/tech/techswitching/TechSwitchingApplication.java
@@ -0,0 +1,13 @@
+package tea.tech.techswitching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSwitchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSwitchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tea/tech/techswitching/TechSwitchingApplicationTests.java
@@ -0,0 +1,13 @@
+package tea.tech.techswitching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSwitchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
